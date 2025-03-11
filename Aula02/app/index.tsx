416beca32aaef31672f45fb5ac806c3b8c3bd616
Adilson-Kina <adilsonkinakina@gmail.com>
2025-03-11 08:55:20 -0300

    Image Uploaded Showing

--- Aula02/app/index.tsx
+++ Aula02/app/index.tsx
@@ -1,31 +1,42 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button, Image } from "react-native";
 import { useState } from 'react';
-import * ImagePicker from 'expo-image-picker';
+import * as ImagePicker from 'expo-image-picker';
 
-/* I did not use async function nor await if not work try again with it
-* What to use as docs -> https://docs.expo.dev/versions/latest/sdk/imagepicker/
-* */
-export default function Index() {
+export default function PickingImage() {
   const [image, setImage] = useState(null);
-  const pickImage = () => {
-    let result = ImagePicker.launchImageLibraryAsync({
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
       allowsEditing: false,
       quality: 1
     })
+
+    console.log(`ação foi cancelada:${result.canceled}`);
+
+    if(!result.canceled){
+      setImage(result.assets[0].uri);
+    }
   }
 
   return (
     <View style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+      <Button title="Choose image" color="#c6a0f6" onPress={pickImage} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container:{
+    backgroundColor: "#24273a",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  }
-})
+  },
+  image:{
+    marginBottom: 20,
+    width: 400,
+    height: 400,
+    borderRadius: 10,
+  },
+});
